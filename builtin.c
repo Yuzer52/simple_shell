418365f20c56acd46074e28cf06ac877debe3b79
Osalive <125866691+Osalive@users.noreply.github.com>
2023-12-13 22:33:43 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _myexit - It exits the shell
+ * _myexit - exits the shell
  * @info: Structure containing potential arguments
  *
  *  Return: exits with a given exit status
